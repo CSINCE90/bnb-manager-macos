5115b873aafc114ebf9499ac17c578f5c79d8ef9
CSINCE90 <158275180+CSINCE90@users.noreply.github.com>
2025-08-27 15:49:51 +0200

    Initial Commit

---
+++ MyBnB/MyBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBnBApp.swift
+//  MyBnB
+//
+//  Created by Francesco Chifari on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBnBTests/MyBnBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBnBTests.swift
+//  MyBnBTests
+//
+//  Created by Francesco Chifari on 27/08/25.
+//
+
+import Testing
+@testable import MyBnB
+
+struct MyBnBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
